1e3f364b2f1dc68755c50e4de621688c79d96e11
chayapomchai <112763767+chayapomchai@users.noreply.github.com>
2022-10-09 22:03:51 +0700

    Update directory_fuzzing.py

--- directory_fuzzing.py
+++ directory_fuzzing.py
@@ -1,24 +1,26 @@
-import requests
 #print(requests.get('https://xshot.winwin.co.th/Main'))
-from requests import get
-import pandas as pd
-import time 
-file = open("/usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt")
-f = pd.DataFrame(file,columns=['name'])
-#print(f)
-print(len(f))
-print("start here")
-a = 'http://10.10.41.157/'
-l = []
+from requests import get 
+file = open("/usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt").read().splitlines()
+file = [f for f in file if f != ""]
+file = [k for k in file if k[0] != "#"]
+#print(file)
+url = 'http://10.10.47.114/'
+#l = []
 def status(x):
     x = get(x).status_code
     return x
-print(status(a))
-for i in f.loc[14:200]['name']:
-    print(f"{get(a+i)} from {i}")
-    if status(a+i) == 429:
-        time.sleep(4)
-        print(f"{get(a+i)} from {i}")
-    if status(a+i) == 200:
-        l.append(i)
-print(l)
+#print(status(a))
+print("start here")
+for i in file:
+#    print(f"{get(a+i)} from {i}")
+#    if status(a+i) == 429:
+#        time.sleep(4)
+#        print(f"{get(a+i)} from {i}")
+    rdurl = url + i
+    if status(rdurl) == 200:
+        print(rdurl)
+#        l.append(i)
+#print(l)
+
+#l = [i for i in file[14:1000] if status(a+i) == 200]
+#print(l)
