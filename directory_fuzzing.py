0e56cefdda1d6bb8a8f5a8ea549cf66ee23d09a5
chayapomchai <112763767+chayapomchai@users.noreply.github.com>
2022-10-14 11:02:32 -0400

    Update directory_fuzzing.py

--- directory_fuzzing.py
+++ directory_fuzzing.py
@@ -1,26 +1,27 @@
-#print(requests.get('https://xshot.winwin.co.th/Main'))
-from requests import get 
-file = open("/usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt").read().splitlines()
+from requests import get
+import optparse
+def getarg():
+    parser = optparse.OptionParser()
+    parser.add_option("-u","--url",dest="url",help="URL of Website")
+    parser.add_option("-w","--wdlt",dest="wordlist",help="The Wordlist Address")
+    option,argument = parser.parse_args()
+    if (not option.url or not option.wordlist):
+        parser.error("[-] Please specify an interface. use --help for more information.")
+    return option
+option = getarg()
+url = option.url
+wordlist = option.wordlist
+file = open(wordlist).read().splitlines()
+#url = 'http:// /'
+#file = open("/usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt").read().splitlines()
 file = [f for f in file if f != ""]
-file = [k for k in file if k[0] != "#"]
+file = [f for f in file if f[0] != "#"]
 #print(file)
-url = 'http://10.10.47.114/'
-#l = []
 def status(x):
     x = get(x).status_code
     return x
-#print(status(a))
 print("start here")
 for i in file:
-#    print(f"{get(a+i)} from {i}")
-#    if status(a+i) == 429:
-#        time.sleep(4)
-#        print(f"{get(a+i)} from {i}")
     rdurl = url + i
-    if status(rdurl) == 200:
+    if get(rdurl).status_code == 200:
         print(rdurl)
-#        l.append(i)
-#print(l)
-
-#l = [i for i in file[14:1000] if status(a+i) == 200]
-#print(l)
